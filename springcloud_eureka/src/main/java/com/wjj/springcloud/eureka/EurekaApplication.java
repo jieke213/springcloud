1a32503aabff2593457cef0178ac52f41b484c30
WangJingjie <wangjjj@sunits.com>
2019-05-07 16:21:29 +0800

    添加了eureka注册中心

---
+++ springcloud_eureka/src/main/java/com/wjj/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.wjj.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Author:   WangJingjie
+ * Date:     2019/5/7 15:17
+ * Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
